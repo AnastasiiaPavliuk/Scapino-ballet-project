729c86694d99a9233460179530f5ea7130b36117
AnastasiiaPavliuk <pavliuknst@gmail.com>
2024-06-05 13:44:35 +0200

    Resolved merge conflicts

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -15,12 +15,11 @@
 #define finishEchoPin 6
 
 float armDistance, presenceDistance, finishDistance;
-bool playerIs;
-int minimalDistance = 13;
+bool playerIs = false;
+// int minimalDistance = 13;
 
 void setup() {
   Serial.begin(9600);
-
   pinMode(led_1, OUTPUT);
   pinMode(led_2, OUTPUT);
   pinMode(buzzer, OUTPUT);
@@ -40,40 +39,14 @@ void loop() {
   presenceDistance = measureDistance(presenceTrigPin, presenceEchoPin);
   finishDistance = measureDistance(finishTrigPin, finishEchoPin);
 
- 
-
-
-    Serial.print("Arm Distance: ");
-  Serial.println(armDistance);
-  
-  Serial.print("Presence Distance: ");
-  Serial.println(presenceDistance);
-  
-  Serial.print("Finish Distance: ");
-  Serial.println(finishDistance);
-
-  Serial.print("playerIs: ");
-  Serial.println(playerIs);
-
-
-
-    if (finishDistance < minimalDistance) {
-      Serial.print("finishDistance in if st: ");
-      Serial.println(finishDistance);
-      Serial.print("minimalDistance in if st : ");
-      Serial.println(minimalDistance);
-      playerIs = false;
-    }
-
     DynamicJsonDocument doc(256);
 
-      doc["playerIs"] = playerIs;
-
       if (playerIs) {
-      // DynamicJsonDocument doc(256);
+
       doc["armDistance"] = armDistance;
       doc["presenceDistance"] = presenceDistance;
       doc["finishDistance"] = finishDistance;
+      // doc["playerIs"] = playerIs;
       serializeJson(doc, Serial);
       Serial.println();
 
@@ -86,15 +59,19 @@ void loop() {
 
   if (Serial.available() > 0) {
     //read data
-    String input = Serial.readStringUntil('\n');
+    String s = Serial.readStringUntil('\n');
     StaticJsonDocument<256> doc;
-    DeserializationError error = deserializeJson(doc, input);
+    DeserializationError error = deserializeJson(doc, s);
+
     if (error) {
-      Serial.print(F("deserializeJson() failed: "));
-      Serial.println(error.c_str());
+      logSerial("json parse failed");
       return;
     }
-
+ 
+    if (doc.containsKey("playerIs")) {
+      //receive that player is true
+      playerIs = doc["playerIs"].as<bool>();
+    }
   }
 }
 
@@ -109,31 +86,14 @@ float measureDistance(int trigPin, int echoPin) {
   return (duration * 0.0343) / 2;  // Convert time to distance
 }
 
-void checkFinishSensor() {
-
-  Serial.print("finishDistance: ");
-  Serial.println(finishDistance);
-
-  if (finishDistance < minimalDistance) {
-    playerIs = false;
-  }
+void logSerial(const char* message) {
+  DynamicJsonDocument doc(256);
+  doc["type"] = "message";
+  doc["value"] = message;
+  serializeJson(doc, Serial);
+  Serial.println();
 }
 
-void handleDistance(float distance) {
-  // if (distance < 10) {
-  //   digitalWrite(led_1, HIGH);
-  //   digitalWrite(led_2, HIGH);
-  // } else {
-  //   digitalWrite(led_1, LOW);
-  //   digitalWrite(led_2, LOW);
-  // }
-
-  if (distance < 3) {
-    tooClose(distance);
-  } else {
-    noTone(buzzer);
-  }
-}
 
 void tooClose(float distance) {
   tone(buzzer, 100);
